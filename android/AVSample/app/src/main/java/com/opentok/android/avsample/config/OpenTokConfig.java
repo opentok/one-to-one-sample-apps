d2910acd74c3841e71d1fa3b1722edc99e287ce7
marinas <marinaserranomontes@gmail.com>
2016-02-09 17:49:17 +0100

    Cleaning authentication values and unused libs

--- android/AVSample/app/src/main/java/com/opentok/android/avsample/config/OpenTokConfig.java
+++ android/AVSample/app/src/main/java/com/opentok/android/avsample/config/OpenTokConfig.java
@@ -5,11 +5,11 @@ public class OpenTokConfig {
     // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
     // ***                      https://dashboard.tokbox.com/projects                           ***
     // Replace with a generated Session ID
-    public static final String SESSION_ID = "2_MX4xMDB-flR1ZSBOb3YgMTkgMTE6MDk6NTggUFNUIDIwMTN-MC4zNzQxNzIxNX4";
+    public static final String SESSION_ID = "";
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    public static final String TOKEN = "T1==cGFydG5lcl9pZD0xMDAmc2RrX3ZlcnNpb249dGJwaHAtdjAuOTEuMjAxMS0wNy0wNSZzaWc9NTMzNDI5Y2ZhNjIxZWY4NDgxN2ViNTgzMTIwMDUyMjMwZjhhN2E5NDpzZXNzaW9uX2lkPTJfTVg0eE1EQi1mbFIxWlNCT2IzWWdNVGtnTVRFNk1EazZOVGdnVUZOVUlESXdNVE4tTUM0ek56UXhOekl4Tlg0JmNyZWF0ZV90aW1lPTE0NTQ1NzY1OTAmcm9sZT1tb2RlcmF0b3Imbm9uY2U9MTQ1NDU3NjU5MC42NTE3MTc4NTU1MDE2OSZleHBpcmVfdGltZT0xNDU3MTY4NTkw";
+    public static final String TOKEN = "";
     // Replace with your OpenTok API key
-    public static final String API_KEY = "100";
+    public static final String API_KEY = "";
 
     // Subscribe to a stream published by this client. Set to false to subscribe
     // to other clients' streams only.

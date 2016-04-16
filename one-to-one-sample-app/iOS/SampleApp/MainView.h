eed12c6464fa1f1438bf116074acaf1646d672b5
Lucas Huang <lucas@tokbox.com>
2016-04-15 21:43:28 -0700

    use SVProgressHUD as alerts

--- one-to-one-sample-app/iOS/SampleApp/MainView.h
+++ one-to-one-sample-app/iOS/SampleApp/MainView.h
@@ -35,12 +35,5 @@
 - (void)hideSubscriberControls;
 
 // other controls
-- (void)showConnectingLabel;
-- (void)hideConnectingLabel;
-
-- (void)showErrorMessageLabelWithMessage:(NSString *)message
-                            dismissAfter:(CGFloat)seconds;
-- (void)hideErrorMessageLabel;
-
 - (void)removePlaceHolderImage;
 @end

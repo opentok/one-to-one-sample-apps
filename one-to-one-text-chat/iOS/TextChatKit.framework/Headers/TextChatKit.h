2eb2f0ece27a2b02b96e1c4c020413f605ab3dc5
Lucas Huang <lucas@tokbox.com>
2016-05-18 12:01:02 -0700

    update for iOS documentation

--- one-to-one-text-chat/iOS/TextChatKit.framework/Headers/TextChatKit.h
+++ one-to-one-text-chat/iOS/TextChatKit.framework/Headers/TextChatKit.h
@@ -6,7 +6,6 @@
 //  Copyright © 2016 Tokbox. All rights reserved.
 //
 
-//#import <OTAcceleratorPackUtil/OTAcceleratorSession.h>
 #import <TextChatKit/TextChat.h>
 #import <TextChatKit/TextChatView.h>
 #import <TextChatKit/TextChatUICustomizator.h>
--- one-to-one-text-chat/iOS/TextChatKit.framework/Headers/TextChatUICustomizator.h
+++ one-to-one-text-chat/iOS/TextChatKit.framework/Headers/TextChatUICustomizator.h
@@ -8,20 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-static NSString * const TextChatUIUpdatedNotificationName = @"TextChatUIUpdatedNotificationName";
-
 @interface TextChatUICustomizator : NSObject
 
+/**
+ *  Custom color for the send message text
+ */
 @property (nonatomic) UIColor *tableViewCellSendTextColor;
-
+/**
+ *  Custom color for the received message text
+ */
 @property (nonatomic) UIColor *tableViewCellReceiveTextColor;
-
+/**
+ *  Custom background color for the send message
+ */
 @property (nonatomic) UIColor *tableViewCellSendBackgroundColor;
-
+/**
+ *  Custom background color for the received message
+ */
 @property (nonatomic) UIColor *tableViewCellReceiveBackgroundColor;
-
+/**
+ *  Custom color for the background color of the top bar of the TextChat
+ */
 @property (nonatomic) UIColor *topBarBackgroundColor;
-
+/**
+ *  Custom color for the text title in the top bar of the TextChat
+ */
 @property (nonatomic) UIColor *topBarTitleTextColor;
 
 @end

9e2e2e19d74c4e4ef6a381655aa20bb08be7212b
marinas <marinaserranomontes@gmail.com>
2016-06-14 16:58:23 +0200

    SS android sample

---
+++ one-to-one-screensharing-annotations/android/OneToOneScreenSharingSample/app/src/main/java/com/tokbox/android/screensharingsample/config/OpenTokConfig.java
@@ -0,0 +1,29 @@
+package com.tokbox.android.screensharingsample.config;
+
+public class OpenTokConfig {
+
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+    public static final String SESSION_ID = "1_MX40NTU4OTAyMn5-MTQ2NDc5NzE5ODUzOH5HazV0QnpCdE9zbVB3cEtuR0hnZGpYUnV-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTU4OTAyMiZzaWc9YmI5NTQ3OTA4MmQ3ODg1Nzg5NDNhNmEwNzkzNzRjNGI3ODNjYjhhNTpzZXNzaW9uX2lkPTFfTVg0ME5UVTRPVEF5TW41LU1UUTJORGM1TnpFNU9EVXpPSDVIYXpWMFFucENkRTl6YlZCM2NFdHVSMGhuWkdwWVVuVi1mZyZjcmVhdGVfdGltZT0xNDY0Nzk3MjExJm5vbmNlPTAuNTMxODE2OTY2MzY2MDIyOCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDY3Mzg5MjEw";
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45589022";
+
+    // Subscribe to a stream published by this client. Set to false to subscribe
+    // to other clients' streams only.
+    public static final boolean SUBSCRIBE_TO_SELF = false;
+
+    // For internal use only. Please do not modify or remove this code.
+    public static final String LOG_CLIENT_VERSION = "android-vsol-1.0.0";
+    public static final String LOG_SOURCE = "one_to_one_screensharing_sample_app";
+    public static final String LOG_ACTION_INITIALIZE = "initialize";
+    public static final String LOG_ACTION_START_COMM = "start_comm";
+    public static final String LOG_ACTION_END_COMM = "end_comm";
+
+    public static final String LOG_VARIATION_ATTEMPT = "Attempt";
+    public static final String LOG_VARIATION_ERROR = "Failure";
+    public static final String LOG_VARIATION_SUCCESS = "Success";
+
+
+}

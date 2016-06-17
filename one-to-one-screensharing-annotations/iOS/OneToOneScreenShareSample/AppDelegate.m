1f29dc3168194bf763088c7de69e2d745874844c
marinas <marinaserranomontes@gmail.com>
2016-06-17 12:33:32 +0200

    Removing credentials from iOS sample

--- one-to-one-screensharing-annotations/iOS/OneToOneScreenShareSample/AppDelegate.m
+++ one-to-one-screensharing-annotations/iOS/OneToOneScreenShareSample/AppDelegate.m
@@ -18,9 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [OneToOneCommunicator setOpenTokApiKey:@"45589022"
-                                 sessionId:@"1_MX40NTU4OTAyMn5-MTQ2NDc5NzE5ODUzOH5HazV0QnpCdE9zbVB3cEtuR0hnZGpYUnV-fg"
-                                     token:@"T1==cGFydG5lcl9pZD00NTU4OTAyMiZzaWc9YmI5NTQ3OTA4MmQ3ODg1Nzg5NDNhNmEwNzkzNzRjNGI3ODNjYjhhNTpzZXNzaW9uX2lkPTFfTVg0ME5UVTRPVEF5TW41LU1UUTJORGM1TnpFNU9EVXpPSDVIYXpWMFFucENkRTl6YlZCM2NFdHVSMGhuWkdwWVVuVi1mZyZjcmVhdGVfdGltZT0xNDY0Nzk3MjExJm5vbmNlPTAuNTMxODE2OTY2MzY2MDIyOCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDY3Mzg5MjEw"];
+    [OneToOneCommunicator setOpenTokApiKey:@""
+                                 sessionId:@""
+                                     token:@""];
     return YES;
 }
 
